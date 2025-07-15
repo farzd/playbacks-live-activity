1ab37697ddc76cab6aa12be3e49030f90a94fe03
Farzad <farzad.qasim@gmail.com>
2025-07-15 21:09:20 +0700

    Add activityId parameter to CompleteIntent for enhanced functionality and initialization. Update perform method to support new parameter.

--- ios-files/CompleteIntent.swift
+++ ios-files/CompleteIntent.swift
@@ -6,8 +6,15 @@ struct CompleteIntent: AppIntent, LiveActivityIntent {
     static var title: LocalizedStringResource = "Complete Recording"
     static var description: IntentDescription = "Opens the app to complete the recording"
     static var openAppWhenRun: Bool = true
+    
+    @Parameter(title: "Activity ID")
+    var activityId: String?
 
     init() {}
+    
+    init(activityId: String?) {
+        self.activityId = activityId
+    }
     
     func perform() async throws -> some IntentResult {
         print("CompleteIntent perform() called")
